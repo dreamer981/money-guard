adc12db1c52c7fd1a2d0f3ba226d634e562818bd
Nilay Türker <nilaytr89qgmail.com>
2025-05-06 00:57:59 +0300

    dashboard hometab ve statisticstab düzenlendi

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,8 +1,41 @@
+import Header from "../../components/Header/Header";
+import TransActionsList from "../../components/TransactionsList/TransactionsList";
+import StatisticsDashboard from "../../components/StatisticsDashboard/StatisticsDashboard";
+import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
+import Chart from "../../components/Chart/Chart";
+import Currency from "../../components/Currency/Currency";
+
 export default function DashboardPage() {
-    return (
+  return (
+      <>
       <div>
         <h1>Dashboard Page</h1>
       </div>
+      <section>
+            <h2>Header</h2>
+            <Header />
+      </section>
+      <section>
+            <h2>TransActions List</h2>
+            <TransActionsList />
+      </section>
+      <section>
+            <h2>Statistics Dashboard</h2>
+            <StatisticsDashboard />
+      </section>
+      <section>
+            <h2>Statistics Table</h2>
+            <StatisticsTable />
+      </section>
+      <section>
+            <h2>Chart</h2>
+            <Chart />
+      </section>
+      <section>
+            <h2>Currency</h2>
+            <Currency />
+      </section>
+      </>
     );
   }
   
--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,8 +1,26 @@
+import TransActionsList from "../../components/TransactionsList/TransactionsList";
+import TransactionsItem from "../../components/TransactionsItem/TransactionsItem";
+import ButtonAddTransactions  from "../../components/ButtonAddTransactions/ButtonAddTransactions";
+
 export default function HomeTab() {
-    return (
+  return (
+      <>
       <div>
         <h1>Home Tab</h1>
       </div>
+      <section>
+            <h2>Transactions List</h2>
+            <TransActionsList />
+      </section>
+      <section>
+            <h2>Transactions Item</h2>
+            <TransactionsItem />
+      </section>
+      <section>
+            <h2>Button Add Transactions</h2>
+            <ButtonAddTransactions />
+      </section>
+      </>
     );
-  }
+}
   
