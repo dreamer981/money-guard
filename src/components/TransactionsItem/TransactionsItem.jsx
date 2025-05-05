576081d5a0aba290203a085a557e71bb91b22b36
dreamer981 <sdreamer590@gmail.com>
2025-05-05 19:51:19 +0300

    TransactionItem düzenlendi

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,34 +1,39 @@
-import css from "./TransactionsItem.module.css";
+import { useState } from "react";
+import ModalEditTransaction from "./ModalEditTransaction"; 
 import { useDispatch } from "react-redux";
-import {
-  updateTransaction,
-  deleteTransaction,
-} from "../../redux/transactions/operations";
+import { deleteTransaction } from "../redux/transactions/operations";
 
 const TransactionsItem = ({ transaction }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false); 
   const dispatch = useDispatch();
 
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   const handleDelete = () => {
-    dispatch(deleteTransaction(transaction.id));
+    dispatch(deleteTransaction(transaction.id)); 
   };
 
-  const handleUpdate = () => {
-    const updatedTransaction = { ...transaction, status: "completed" };
-    dispatch(
-      updateTransaction({
-        transactionId: transaction.id,
-        transaction: updatedTransaction,
-      })
-    );
-  };
   return (
-    <div className={css.transactionItem}>
-      <p>{transaction.description}</p>
-      <p>{transaction.amount}</p>
-      <p>{transaction.date}</p>
-      <button onClick={handleUpdate}>Update</button>
-      <button onClick={handleDelete}>Delete</button>
+    <div className="transaction-item">
+      <p>Date: {transaction.date}</p>
+      <p>Type: {transaction.type}</p>
+      <p>Category: {transaction.category}</p>
+      <p>Comment: {transaction.comment}</p>
+      <p>Sum: {transaction.amount}</p>
+
+      <button onClick={openModal}>Edit</button> {/* Modal açma butonu */}
+      <button onClick={handleDelete}>Delete</button> {/* Silme butonu */}
+
+      {/* ModalEditTransaction modal bileşeni */}
+      {isModalOpen && (
+        <ModalEditTransaction
+          transaction={transaction}
+          closeModal={closeModal}
+        />
+      )}
     </div>
   );
 };
+
 export default TransactionsItem;
