daf04b608c4935fac788920e903e9b814141c824
your_name <orkunkorcak@gmail.com>
2025-05-07 00:59:03 +0300

    home tab düzeltildi

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -40,31 +40,3 @@ export default function HomeTab() {
     </div>
   );
 }
-
-import TransActionsList from "../../components/TransactionsList/TransactionsList";
-import TransactionsItem from "../../components/TransactionsItem/TransactionsItem";
-import ButtonAddTransactions  from "../../components/ButtonAddTransactions/ButtonAddTransactions";
-
-export default function HomeTab() {
-  return (
-      <>
-      <div>
-        <h1>Home Tab</h1>
-      </div>
-      <section>
-            <h2>Transactions List</h2>
-            <TransActionsList />
-      </section>
-      <section>
-            <h2>Transactions Item</h2>
-            <TransactionsItem />
-      </section>
-      <section>
-            <h2>Button Add Transactions</h2>
-            <ButtonAddTransactions />
-      </section>
-      </>
-    );
-}
-  
-
